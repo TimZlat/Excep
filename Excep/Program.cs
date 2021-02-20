200b242c5d31cfb20199eedfd0fa4ce4f743d8a4
Sav Raska <nat.ahmerova@gmail.com>
2021-02-20 13:49:48 +0300

    Добавьте файлы проекта.

---
+++ Excep/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Excep
+{
+    // Обработка исключенией
+    class Program
+    {
+        static void M1(int x, int y, int z)
+        {
+            try
+            {
+                int a = checked((int)Math.Pow(x, y));
+                Console.WriteLine("x^y/z = {0}", a / z);
+
+            }
+            catch  (DivideByZeroException)
+            {
+                Console.WriteLine("DivideByZero Exception detected!!");
+            }
+            Console.WriteLine("M1 finished");
+        }
+        static void M2(int x, int y, int z)
+        {
+            try
+            {
+                M1(x, y, z);
+            }
+            catch (ArithmeticException)
+            {
+                Console.WriteLine("Arithmetic Exception detected");
+            }
+            Console.WriteLine("M2 finished");
+        }
+    
+        static void Main(string[] args)
+        {
+            Console.Write("x = ");
+            int x = int.Parse(Console.ReadLine());
+            Console.Write("y = ");
+            int y = int.Parse(Console.ReadLine());
+            Console.Write("z = ");
+            int z = int.Parse(Console.ReadLine());
+            M2(x, y, z);
+            Console.WriteLine("Для завершения нажмите любую клавишу ...");
+            Console.ReadKey();
+        }
+    }
+}
